d65d91f5ff9eebb343059a26745422d31e17a7bf
Klevest Palucaj <klevestpalucaj@gmail.com>
2022-08-01 21:33:59 +0300

    fixed the app js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-import Header from './Layout/Header/Header';
+import React from "react";
+import "./App.css";
+import Header from "./Layout/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Main from './Layout/Main/Main';
-import Footer from './Layout/Footer/Footer';
+import Main from "./Layout/Main/Main";
+import Footer from "./Layout/Footer/Footer";
 
 const App = () => {
   return (
@@ -12,6 +13,6 @@ const App = () => {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
